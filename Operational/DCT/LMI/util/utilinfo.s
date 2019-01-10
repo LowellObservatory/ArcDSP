e19fc0bdc161cac4c26bc73e5833d6397778175a
Dyer M Lytle <dlytle@lowell.edu>
2019-01-10 09:53:45 -0700

    Add files via upload

---
+++ Operational/DCT/LMI/util/utilinfo.s
@@ -0,0 +1,11 @@
+; DSP Version
+IVERSION	EQU	278655	; 1.4/1
+
+IFLAVOR	EQU	'   '	 ; none 
+
+ITIME0	EQU	13674  	 ; lo order time: 2012 5 22  6:42:50 GMT
+
+ITIME1	EQU	20411  	 ; hi order time: 2012 5 22  6:42:50 GMT
+
+ISVNREV	EQU	0	 ; most recent code svn rev
+
