d267e8326f8e0b5e413e111cb1f723a15ac48fff
Dyer M Lytle <dlytle@lowell.edu>
2019-03-18 13:39:09 -0700

    Add files via upload

---
+++ Operational/DCT/PCI/pciinfo.s
@@ -0,0 +1,11 @@
+; DSP Version
+IVERSION	EQU	291269	; 1.7/7/5
+
+IFLAVOR	EQU	'   '	 ; none 
+
+ITIME0	EQU	7201  	 ; lo order time: 2011 2 26  15:28:33 GMT
+
+ITIME1	EQU	19817  	 ; hi order time: 2011 2 26  15:28:33 GMT
+
+ISVNREV	EQU	0	 ; most recent code svn rev
+
---
+++ Operational/DCT/PCI/pciinfospec.s
@@ -0,0 +1,5 @@
+; PCI DSP info field specifications.
+; These values are 'addresses' and used as the argument for the INF command.
+
+GET_CAPABLE	EQU	$100	; ICAPABLE field (what dsp supports).
+
